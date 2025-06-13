6fa81e45a93d3cd0b4e41cbe97290904f0890bbc
bhuvan <bhuvanannappa@gmail.com>
2025-06-13 12:06:12 +0530

    done

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs= require("fs");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  fs.readdir("./files",(err,files)=>{
+
+    res.render("index",{files:files})
+  })
+});
+
+app.post("/create",(req,res)=>{
+  fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`,req.body.details,(err)=>{
+    res.redirect("/");
+  })
+})
+
+app.get("/files/:fileName", (req, res) => {
+  fs.readFile(`./files/${req.params.fileName}`,"utf-8",(err,fileData)=>{
+    res.render("readMore",{fileName : req.params.fileName, fileData:fileData});
+  })
+})
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
